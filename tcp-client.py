051f07974c9c33f2be7809adfecbbaa7c9ce95cd
Colby Hillman <colby.hillman@gmail.com>
2020-03-11 01:39:20 -0400

    Update tcp-client.py

--- tcp-client.py
+++ tcp-client.py
@@ -1,45 +1,49 @@
 import struct
 import asyncio
 
+async def receive_str(reader):
+    length = await reader.read(struct.calcsize('<i'))
+    length = struct.unpack('<i', length)[0]
+    string = await reader.read(length)
+    string = string.decode()
+    return string
+
+async def send_str(string,writer):
+    string = string.encode()
+    data = struct.pack('<i', len(string)) 
+    data += struct.pack(str(len(string))+'s', string)
+    writer.write(data)
+    await writer.drain()
+
 async def receive_messages(reader):
-    length = await reader.read(struct.calcsize('<i'))
-    length = struct.unpack('<i', length)[0]
-    name = await reader.read(length)
-    name = name.decode()
+    name = await receive_str(reader)
+    message = await receive_str(reader)
+    time = await receive_str(reader)
 
+    print(name,"-",time+":",message)
 
-    length = await reader.read(struct.calcsize('<i'))
-    length = struct.unpack('<i', length)[0]
-    message = await reader.read(length)
-    message = message.decode()
-    
-
-    length = await reader.read(struct.calcsize('<i'))
-    length = struct.unpack('<i', length)[0]
-    time = await reader.read(length)
-    time = time.decode()
-
-    print(name,",",time,":",message)
+async def send_message(writer,message):
+    if not message:
+        writer.close()
+        return writer
+    await send_str(message,writer)
+    return writer
 
 async def get_messages(writer,reader):
     loop = asyncio.get_running_loop()
-    while True:
+
+    while not writer.is_closing():
         try:
-            name, message, time = await receive_messages(reader)
-        except:
-            pass
-        message = await loop.run_in_executor(None,input)
-        if not message:
-            writer.close()
-            break
-        message = message.encode()
-        data = struct.pack('<i', len(message))
-        data += struct.pack(str(len(message))+'s', message)
-        writer.write(data)
-        await writer.drain()
+            await receive_messages(reader)
+        finally:
+            try:
+                message = await loop.run_in_executor(None,input)
+                writer = await send_message(writer,message)
+            except:
+                pass
 
 async def tcp_client():
-    reader, writer = await asyncio.open_connection("10.77.128.114", 25565)
+    reader, writer = await asyncio.open_connection("apache.cslab.moravian.edu", 25565)
     name_taken = True
 
     #checking the  version number 
@@ -48,7 +52,6 @@ async def tcp_client():
     await writer.drain()
     version_verify = await reader.read(struct.calcsize("<?"))
     version_verify = struct.unpack("<?", version_verify)[0]
-    print(version_verify)
     if not version_verify:
         print("Outdated Version")
         writer.close()
@@ -56,18 +59,12 @@ async def tcp_client():
     #getting the username from the client
     while name_taken:
         username = input("Enter a username: ")
-        username = username.encode()
-        data = struct.pack('<i', len(username))
-        data += struct.pack(str(len(username))+'s', username)
-        writer.write(data)
-        await writer.drain()
+        await send_str(username,writer)
 
         #checks to make sure the name is not taken 
         #if name is taken it will ask again for 
         name_taken = await reader.read(struct.calcsize('<?'))
-        name_taken = struct.unpack('<?', name_taken)[0]
-    print(name_taken)
-    
+        name_taken = struct.unpack('<?', name_taken)[0]    
     
     number_of_messages = await reader.read(struct.calcsize('<i'))
     number_of_messages = struct.unpack('<i', number_of_messages)[0]
@@ -78,6 +75,3 @@ async def tcp_client():
     asyncio.create_task(get_messages(writer,reader))   
         
 asyncio.run(tcp_client())
-    
-    
-    
