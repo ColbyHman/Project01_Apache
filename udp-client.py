ceb67bc1f703e7679daa52051d3d7708839f4747
frekotj <60444898+frekotj@users.noreply.github.com>
2020-03-11 20:10:15 -0400

    Add files via upload

--- udp-client.py
+++ udp-client.py
@@ -4,18 +4,33 @@ from time import gmtime, strftime, sleep
 time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 UDP_IP = "127.0.0.1"
 UDP_PORT = 25565
-name = input(" Enter a name: ")
+name = input("Enter a name: ")
 message = input("Enter a message: ")
 name= str.encode(name)
 message = str.encode(message)
+time = str.encode(time)
 
 print(name, message, time)
+
+history = []
+
+
+
+def add_message_to_history(message):
+    """
+    This function removes the oldest message from the history list and appends the latest message.
+    """
+    if len(history) == 10:
+        history.pop(0)
+    history.append(message)
 
 
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.sendto(name, message, (UDP_IP, UDP_PORT))
+client_socket.sendto(name,(UDP_IP, UDP_PORT))
+client_socket.sendto(message,(UDP_IP, UDP_PORT))
 client_socket.sendto(time, (UDP_IP, UDP_PORT))
+client_socket.sendto(history, (UDP_IP, UDP_PORT))
 
 
 
