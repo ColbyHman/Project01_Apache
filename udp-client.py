edf2e9cc7bfe2da98fcf2c60d58408e242863dda
Colby Hillman <colby.hillman@gmail.com>
2020-03-11 20:41:43 -0400

    Update udp-client.py

--- udp-client.py
+++ udp-client.py
@@ -1,43 +1,88 @@
 import socket 
+import asyncio
 from time import gmtime, strftime, sleep
 
-time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-UDP_IP = "127.0.0.1"
-UDP_PORT = 25565
-name = input("Enter a name: ")
-message = input("Enter a message: ")
-name= str.encode(name)
-message = str.encode(message)
-time = str.encode(time)
-
-print(name, message, time)
-
-history = []
+port = 25566
 
 
+class UDPChatProgram(asyncio.DatagramProtocol):
+    name = ""
+    def __init__(self):
+        """
+        This is the constructor for the Chat Program itself
+        """
+        self.on_con_lost = asyncio.get_running_loop().create_future()
+        self.name = input("Enter a username : ")
 
-def add_message_to_history(message):
+    def connection_made(self, transport):
+        """
+        When the connection is created, this will start asking the user for messages
+        """
+        self.transport = transport
+
+        # Starts receiving messages as a task in the asyncio loop
+        asyncio.create_task(self.receive_messages())
+
+    async def receive_messages(self):
+        """
+        Loop forever getting new inputs from the user and then broadcasting them.
+        If the input is the empty string (i.e. just an enter) than it stops the program.
+        """
+        loop = asyncio.get_running_loop()
+        while True:
+            # Get the message from the user
+            message = await loop.run_in_executor(None, input)
+            if not message:
+                self.transport.close()
+                break
+            # Broadcast the message
+            self.transport.sendto(message.encode(), ('255.255.255.255', port))
+
+    def connection_lost(self, exc):
+        """
+        Method called whenever the transport is closed.
+        """
+        self.on_con_lost.set_result(True)
+
+    def datagram_received(self, data, addr):
+        """
+        Method called whenever a datagram is recieved.
+        """
+
+        data = data.decode()
+        if(data == '!!Invalid Username!!'):
+            self.transport.close()
+            return "This username is taken"
+        return data
+
+    def error_received(self, exc):
+        """
+        Method called whenever there is an error with the underlying communication.
+        """
+        print('Error received:', exc)
+
+async def main():
     """
-    This function removes the oldest message from the history list and appends the latest message.
+    CHANGE MAIN TO REFLECT OUR PROTOCOL
     """
-    if len(history) == 10:
-        history.pop(0)
-    history.append(message)
+    # Setup the socket we will be using - enable broadcase and recieve message on the given port
+    # Normally, this wouldn't be necessary, but with broadcasting it is needed
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, True)
+    sock.bind(('', port))
 
+    
+    
+    # Create the transport and protocol with our pre-made socket
+    # If not provided, you would instead use local_addr=(...) and/or remote_addr=(...)
+    loop = asyncio.get_running_loop()
+    transport, protocol = await loop.create_datagram_endpoint(UDPChatProgram, sock=sock)
 
+    # Wait for the connection to be closed/lost
+    try:
+        await protocol.on_con_lost
+    finally:
+        transport.close()
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.sendto(name,(UDP_IP, UDP_PORT))
-client_socket.sendto(message,(UDP_IP, UDP_PORT))
-client_socket.sendto(time, (UDP_IP, UDP_PORT))
-client_socket.sendto(history, (UDP_IP, UDP_PORT))
-
-
-
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind((UDP_IP, UDP_PORT))
-while True:
-    data, addr = server_socket.recvfrom(1024)
-    print(data) 
-
+if __name__ == "__main__":
+    asyncio.run(main())
