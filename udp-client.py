350e14d0cca9366c687608d2fb5ac518ab08ef74
Colby Hillman <colby.hillman@gmail.com>
2020-03-11 21:21:25 -0400

    Update udp-client.py

--- udp-client.py
+++ udp-client.py
@@ -4,16 +4,27 @@ from time import gmtime, strftime, sleep
 
 port = 25566
 
+def get_ip():
+    """Gets the local IP of the current machine."""
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        try:
+            s.connect(('10.255.255.255', 1)) # random IP address, doesn't have to be reachable
+            return s.getsockname()[0] # get the outgoing IP address on the machine
+        except:
+            return '127.0.0.1'
 
 class UDPChatProgram(asyncio.DatagramProtocol):
-    name = ""
+    name = None
     invalid = '!!Invalid Username!!'
+    ip_addr = None
+
     def __init__(self):
         """
         This is the constructor for the Chat Program itself
         """
         self.on_con_lost = asyncio.get_running_loop().create_future()
         self.name = input("Enter a username : ")
+        self.ip_addr = get_ip()
         
 
     def connection_made(self, transport):
@@ -24,9 +35,9 @@ class UDPChatProgram(asyncio.DatagramProtocol):
         name_data = self.name+"||!!"+self.name+"!!"
         self.transport.sendto(name_data.encode(), ('255.255.255.255', port))
         # Starts receiving messages as a task in the asyncio loop
-        asyncio.create_task(self.receive_messages())
+        asyncio.create_task(self.send_messages())
 
-    async def receive_messages(self):
+    async def send_messages(self):
         """
         Loop forever getting new inputs from the user and then broadcasting them.
         If the input is the empty string (i.e. just an enter) than it stops the program.
@@ -54,12 +65,14 @@ class UDPChatProgram(asyncio.DatagramProtocol):
         """
         data = data.decode()
         name, data = data.split('||')
-        if(data == "!!"+self.name+"!!")
-            self.transport.sendto(self.invalid.encode(), ('255.255.255.255', port))
-        elif(data == invalid):
+        if(data == "!!"+self.name+"!!" and self.ip_addr != addr[0]):
+            invalid_name = self.name+"||"+self.invalid
+            self.transport.sendto(invalid_name.encode(), ('255.255.255.255', port))
+        elif(data == self.invalid):
             self.transport.close()
             print("This username is taken")
-        print(name,"-",strftime("%Y-%m-%d %H:%M:%S", gmtime())+":",data)
+        else:
+            print(name,"-",strftime("%Y-%m-%d %H:%M:%S", gmtime())+":",data)
 
     def error_received(self, exc):
         """
